828338430367dad371fc5fb8338f3ae07c6e357e
ggamb <glenngambardella372@gmail.com>
2021-11-10 11:49:56 -0500

    added more files for template

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
-
+const session = require('express-session');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const session = require('express-session');
+
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -30,7 +30,7 @@ const sess = {
 
 app.use(session(sess));
 
-app.engine('handlebars', hbs.engine);
+app.engine('handlebars',  exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // turn on routes
