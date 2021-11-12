38cd0b254f22fa721568c61ed7f6bf3412db00dc
ggamb <glenngambardella372@gmail.com>
2021-11-11 21:24:20 -0500

    added brewery functions for zips and cities. may need updates when put together

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = require('./controllers/');
-const sequelize = require('./config/connection');
+const sequelize = require('./connection');
 const path = require('path');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
