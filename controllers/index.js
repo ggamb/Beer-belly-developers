7d780ed3ae2137be2dfeedeec29eae78084820a2
tygrski <77369211+tygrski@users.noreply.github.com>
2021-11-13 18:13:29 -0600

    seeds file

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 /*Placeholder for server.js*/
 const router = require('express').Router();
 const homeRoutes = require('./HomePage-routes');
+const userRoutes = require ("./User-routes");
 
 router.use('/', homeRoutes);
+router.use('/user', userRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
