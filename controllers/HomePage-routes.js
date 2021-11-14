65f522f49616d64b30c953fe17cc32a4d0e85d7c
ggamb <glenngambardella372@gmail.com>
2021-11-14 14:42:46 -0500

    working zip results. adding additional routes for easier ues

--- controllers/HomePage-routes.js
+++ controllers/HomePage-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 // if needed 
 // redirects user to homepage after login
@@ -13,11 +14,42 @@ router.get('/login', (req, res) => {
 
 
 router.get('/', (req, res) => {
+  
   /*const data = {
     breweries: barNames
   }*/
+  
 
   res.render('index');
+});
+
+router.get('/:zip', (req, res) => {
+  
+  //Searches by entered zip code
+  let zipCode = req.params.zip;
+  const apiKey = "https://api.openbrewerydb.org/breweries?by_postal=" + zipCode;
+
+
+  fetch(apiKey)
+  .then(response => {return response.json()})
+  .then(apiResponse => {
+    //console.log("response", apiResponse);
+    let barNames = {
+      breweries: apiResponse
+    };
+    console.log("bar names inside", barNames)
+    res.render('index', barNames);
+  })
+  .catch(err => {console.log(err)});
+
+  /*console.log("bar names ourside", barNames)
+  const data = {
+    barNames
+  }
+  
+  console.log("data to render", barNames)*/
+
+  
 });
 
 module.exports = router;
--- public/js/api-returns.js
+++ public/js/api-returns.js
@@ -1,36 +1,31 @@
-//const fetch = require('node-fetch');
-
-//Zip code entered by user passed to function
-async function getBreweriesByZip(event) {
-    event.preventDefault();
-    //Searches by entered zip code
-    let zipCode = document.querySelector("#text-zip").value.trim();
-    console.log(zipCode);
-    const apiKey = "https://api.openbrewerydb.org/breweries?by_postal=" + zipCode;
-
-    fetch(apiKey)
-    .then(response => {return response.json()})
-    .then(apiResponse => {
-        console.log(apiResponse);
-        return apiResponse;
-    })
-    .catch(err => {console.log(err)})
-}
-
 //City name entered by user passed to function
 async function getBreweriesByCity (event) {
     //Searches by entered city name
     event.preventDefault();
-    let cityName = document.querySelector("#text-city").value.trim();
-    const apiKey = "https://api.openbrewerydb.org/breweries?by_city=" + cityName;
 
-    fetch(apiKey)
-    .then(response => {return response.json()})
-    .then(apiResponse => {
-        console.log(apiResponse);
-        return apiResponse;
-    })
-    .catch(err => {console.log(err)})
+    let zipCode = document.querySelector("#text-zip").value.trim();
+
+    let currentURL = window.location.href;
+
+    let newURL = currentURL  + "/cities/" + zipCode;
+
+    window.location.href = newURL;
+
+
+}
+
+//Zip code entered by user passed to function
+async function getBreweriesByZip(event) {
+    event.preventDefault();
+
+    let zipCode = document.querySelector("#text-zip").value.trim();
+
+    let currentURL = window.location.href;
+
+    let newURL = currentURL + "/zip/" + zipCode;
+
+    window.location.href = newURL;
+
 }
 
 document.getElementById('submit-city').addEventListener('click', getBreweriesByCity);
