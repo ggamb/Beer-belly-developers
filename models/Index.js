a98a3532b4873679ba525cf529c17a1131fc720c
ggamb <glenngambardella372@gmail.com>
2021-11-21 17:55:38 -0500

    committing node_modules

--- models/Index.js
+++ models/Index.js
@@ -1,8 +1,9 @@
 const User = require("./User");
 const Comment = require("./Comment");
 const BarList = require("./BarList");
-// association / joins
 
+
+// association / joins
 User.hasMany(Comment, {
   foreignKey: "user_id",
   // onDelete: 'SET CASCADE'
@@ -21,17 +22,6 @@ Comment.belongsTo(BarList,{
   through: User,
   foreignKey: "BarList_id",
 });
-
-// User.belongsToMany(BarList, {
-//   through: Comment,
-//   foreignKey: 'user_id',
-//   onDelete: 'SET NULL'
-// });
-
-// BarList.hasMany(User, {
-//   foreignKey: 'user_id',
-//   onDelete: "SET CASCADE"
-// });
 
 
 module.exports = { User, Comment, BarList };
