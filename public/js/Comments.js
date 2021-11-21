ee76afa16bd739dd5157804a5efbcc6c4829df18
ggamb <glenngambardella372@gmail.com>
2021-11-21 15:08:12 -0500

    working comments

--- public/js/Comments.js
+++ public/js/Comments.js
@@ -1,10 +1,8 @@
 async function commentSubmit(event) {
   event.preventDefault();
-  console.log("in comment submit")
+  console.log("in comment submit");
 
-  const comment_text = document
-    .querySelector('textarea[name="comment-body"]')
-    .value.trim();
+  const comment_text = document.getElementById("comment-text").value;
   const bar_id = window.location.toString().split("/")[
     window.location.toString().split("/").length - 1
   ];
@@ -13,11 +11,9 @@ async function commentSubmit(event) {
   console.log('bar_id', bar_id);
 
   if (comment_text) {
-
     const response = await fetch("/api/comment/", {
       method: "post",
       body: JSON.stringify({
-        comment_id,
         comment_text,
         BarList_id : bar_id
       }),
@@ -34,4 +30,4 @@ async function commentSubmit(event) {
   }
 }
 
-document.querySelector("#submit-btn").addEventListener("submit", commentSubmit);
+document.querySelector("#submit-btn").addEventListener('click', commentSubmit);
