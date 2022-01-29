a9ad8da9d2bbe794176ed2f28f17a854fcd9d161
ggamb <glenngambardella372@gmail.com>
2022-01-29 17:14:45 -0500

    change to connection for heroku

--- config/connection.js
+++ config/connection.js
@@ -11,10 +11,10 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 })
 
 // create connection to our db
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+/*const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
-});
+});*/
 
 module.exports = sequelize;
