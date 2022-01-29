06896ac257e67ed26bffcd70f4b2de3ca0e9c56c
ggamb <glenngambardella372@gmail.com>
2022-01-29 17:13:40 -0500

    changes to connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,13 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-if (process.env.DATABASE) {
-  // the application is executed on Heroku ... use the clearDB database
-  const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DATABASE,
-  })
-}
+// the application is executed on Heroku ... use the clearDB database
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DATABASE,
+})
 
 // create connection to our db
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
