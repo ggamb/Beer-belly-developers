b8a8630dd7e62bffb555b9d43549db856513682b
ggamb <glenngambardella372@gmail.com>
2022-02-15 10:42:01 -0500

    pushing to heroku

--- config/connection.js
+++ config/connection.js
@@ -3,18 +3,18 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // the application is executed on Heroku ... use the clearDB database
-/*const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DATABASE,
-})*/
+})
 
 // create connection to our local db
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PW, {
+/*const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PW, {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
-});
+});*/
 
 module.exports = sequelize;
